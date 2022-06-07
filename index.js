e0e063248f9e715b59caef48bd1e511ff1aade7d
Manuel Estrada <manuel.estrada16@gmail.com>
2022-06-07 19:03:11 -0400

    first commit, added loops for expenses

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-// Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses = weeklyExpenses + answer;
+
+}
+
+for (let x = 0; x < monthlyExpenseQuestions.length; x++) {
+
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[x]));
+    monthlyExpenses = monthlyExpenses + answer;
+
+}
+
+for (let y = 0; y < annualExpenseQuestions.length; y++) {
+    
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[y]));
+    annualExpenses = annualExpenses + answer;
+
+}
